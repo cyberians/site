f263630fea35ff9b6dbc4026fb5a433294a1e069
Vladimir Petrachuk <hippozz@yandex.ru>
2016-03-04 10:22:55 +0500

    one

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :services
 	root 'main_page#index'
 
+  match '/services',    to: 'services#index',    via: 'get'
+  match '/service_categories',    to: 'service_categories#index',    via: 'get'
+
 end
